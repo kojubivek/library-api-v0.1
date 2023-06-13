7e3459957fdbde4103d9cb106e9ea0db4cf1992b
kojubivek <kojubivek"gmail.com>
2023-06-14 02:47:58 +1000

    changed port

--- server.js
+++ server.js
@@ -3,53 +3,38 @@ dotenv.config();
 import express from "express";
 import cors from "cors";
 import path from "path";
-const __dirname = path.resolve();
-
 import { ERROR } from "./src/constant.js";
-const app = express();
-const PORT = process.env.NODE_ENV || 8000;
-
-//connect to database
 import { connectDB } from "./src/config/dbConfig.js";
-connectDB();
-
-//middlewares
-app.use(express.json());
-app.use(cors());
-
-// api routers
 import userRouter from "./src/routers/userRouter.js";
 import bookRouter from "./src/routers/bookRouter.js";
 import { isAuth } from "./src/middlewares/authMiddleware.js";
 import transactionRouter from "./src/routers/transactionRouter.js";
 
-app.use(express.static(path.join(__dirname, "/client/build")));
+const app = express();
+const PORT = process.env.PORT || 8000;
+const __dirname = path.resolve();
 
+// Connect to the database
+connectDB();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// API routers
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/book", isAuth, bookRouter);
 app.use("/api/v1/transaction", isAuth, transactionRouter);
 
-app.use("/register", (req, res) => {
-  res.send("OOOPS");
-});
-//serving
-app.get("/", (req, res, next) => {
-  try {
-    res.sendFile(path.join(__dirname, "/client/build/index.html"));
-  } catch (error) {
-    next(error);
-  }
+// Serve static files from the "client/build" directory
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+// Route to serve your static client build
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
-// all uncaught request
-// app.use("*", (req, res) => {
-//   // res.redirect("/");
-//   res.json({
-//     message: "System status is healthy!",
-//   });
-// });
-
-//global error handler
+// Global error handler
 app.use((error, req, res, next) => {
   console.log(error.message);
 
@@ -60,10 +45,13 @@ app.use((error, req, res, next) => {
   });
 });
 
-//run the server
+// Run the server
 app.listen(PORT, (error) => {
-  error
-    ? console.log(error)
-    : console.log(`server is running at http://localhost:${PORT}`);
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`Server is running at http://localhost:${PORT}`);
+  }
 });
+
 export default app;
